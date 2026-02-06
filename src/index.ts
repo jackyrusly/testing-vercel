3166a02790fd0c1afa0c195bb9ae34ece816e668
jackyrusly <jackyrusly@gmail.com>
2026-02-06 16:04:07 +0700

    hello

--- src/index.ts
+++ src/index.ts
@@ -10,18 +10,22 @@ const app = express();
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 app.post('/', async (req, res) => {
     console.log(req.body);
     // const prompt = req.params.prompt;
-res.send("hello world")
-    // const prompt = req.body.prompt;
 
-    // const response = await ai.models.generateContent({
-    //     model: "gemini-3-flash-preview",
-    //     contents: prompt,
-    // }) as any;
+    const prompt = req.body.prompt;
 
-    // res.send(response.candidates[0].content.parts[0].text);
+    const response = await ai.models.generateContent({
+        model: "gemini-3-flash-preview",
+        contents: prompt,
+    }) as any;
+
+    res.send(response.candidates[0].content.parts[0].text);
 })
 
 app.listen(process.env.PORT || 3000);
